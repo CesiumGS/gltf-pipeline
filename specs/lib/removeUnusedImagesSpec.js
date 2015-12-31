e61431bd6e74e7643578e6a32ac26882600beca8
Patrick Cozzi <pjcozzi@gmail.com>
2015-12-31 13:34:01 -0500

    Test for not removing an image

--- specs/lib/removeUnusedImagesSpec.js
+++ specs/lib/removeUnusedImagesSpec.js
@@ -33,4 +33,30 @@ describe('removeUnusedImages', function() {
         expect(gltf.images.unusedId).not.toBeDefined();
         expect(stats.numberOfImagesRemoved).toEqual(1);
     });
+
+    it('does not remove any images', function() {
+        var gltf = {
+            "images": {
+                "Image0001": {
+                    "name": "Image0001",
+                    "uri": "Cesium_Logo_Flat.png"
+                }
+            },
+            "textures": {
+                "texture_Image0001": {
+                    "format": 6408,
+                    "internalFormat": 6408,
+                    "sampler": "sampler_0",
+                    "source": "Image0001",
+                    "target": 3553,
+                    "type": 5121
+                }
+            }
+        };
+
+        var stats = new OptimizationStatistics();
+        removeUnusedImages(gltf, stats);
+        expect(gltf.images.Image0001).toBeDefined();
+        expect(stats.numberOfImagesRemoved).toEqual(0);
+    });
 });
