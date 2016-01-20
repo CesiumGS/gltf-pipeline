4e5677b534c27fad42e881b7f84085c3ec67f038
Richard Lee <agitosan@gmail.com>
2016-01-20 17:18:19 -0500

    Added remove bufferViews stage

--- bin/gltf-pipeline.js
+++ bin/gltf-pipeline.js
@@ -7,6 +7,7 @@ var addDefaults = require('../').addDefaults;
 var removeUnusedImages = require('../').removeUnusedImages;
 var removeUnusedSamplers = require('../').removeUnusedSamplers;
 var removeUnusedShaders = require('../').removeUnusedShaders;
+var removeUnusedBufferViews = require('../').removeUnusedBufferViews;
 var OptimizationStatistics = require('../').OptimizationStatistics;
 var Cesium = require('cesium');
 var defined = Cesium.defined;
@@ -33,6 +34,7 @@ fs.readFile(gltfPath, function (err, data) {
     removeUnusedImages(gltf, stats);
     removeUnusedSamplers(gltf, stats);
     removeUnusedShaders(gltf, stats);
+    removeUnusedBufferViews(gltf, stats);
     addDefaults(gltf, stats);
 
     stats.print();
