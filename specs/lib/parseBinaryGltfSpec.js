265cecda21c104767b03168576c2343af40f8ca7
Richard Lee <agitosan@gmail.com>
2016-02-28 00:34:30 -0500

    Updated parse binary gltf stage

--- specs/lib/parseBinaryGltfSpec.js
+++ specs/lib/parseBinaryGltfSpec.js
@@ -1,40 +1,68 @@
 'use strict';
 var fs = require('fs');
+var async = require('async');
+var bufferEqual = require('buffer-equal');
 var parseBinaryGltf = require('../../lib/parseBinaryGltf');
 var binaryGltfPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.glb';
+var gltfScenePath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTestBinary.txt';
+var fragmentShaderPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest0FS_Binary.glsl';
+var imagePath = './specs/data/boxTexturedUnoptimized/Cesium_Logo_Flat_Binary.png';
 
 describe('parseBinaryGltf', function() {
-    var binaryGltfData;
+    var testData = {
+        binary: binaryGltfPath,
+        gltf: gltfScenePath,
+        shader: fragmentShaderPath,
+        image: imagePath
+    };
 
     beforeAll(function(done) {
-        fs.readFile(binaryGltfPath, function (err, data) {
+        var names = Object.keys(testData);
+        async.each(names, function(name, callback) {
+            fs.readFile(testData[name], function(err, data) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    testData[name] = data;
+                    callback();
+                }
+            });
+        }, function(err) {
             if (err) {
                 throw err;
             }
-            binaryGltfData = data;
             done();
         });
     });
 
-    it('loads a binary gltf file', function(done) {
-        // var gltf = {
-        //     "images": {
-        //         "Image0001": {
-        //             "uri": "Cesium_Logo_Flat_Low.png"
-        //         }
-        //     }
-        // };
+    it('loads a glTF scene', function() {
+        var gltf = parseBinaryGltf(testData.binary);
 
-        // gltf = loadGltfUris(gltf, filePath, function() {
-        //     expect(gltf.images.Image0001.extras.source).toBeDefined();
-        //     expect(bufferEqual(gltf.images.Image0001.extras.source, imageData)).toBe(true);
-        //     expect(gltf.images.Image0001.extras.extension).toEqual('.png');
-        //     done();
-        // });
-        var gltf = parseBinaryGltf(binaryGltfData, function() {
-            done();
-        });
+        expect(gltf).toBeDefined();
+        expect(gltf.buffers.KHR_binary_glTF).toBeDefined();
+        expect(gltf.images.Image0001).toBeDefined();
+        expect(gltf.shaders.CesiumTexturedBoxTest0FS).toBeDefined();
+        expect(gltf.shaders.CesiumTexturedBoxTest0VS).toBeDefined();
+        delete gltf.buffers.KHR_binary_glTF.extras;
+        delete gltf.images.Image0001.extras;
+        delete gltf.shaders.CesiumTexturedBoxTest0FS.extras;
+        delete gltf.shaders.CesiumTexturedBoxTest0VS.extras;
+
+        expect(gltf).toEqual(JSON.parse(testData.gltf));
     });
 
+    it('loads an embedded image', function() {
+        var gltf = parseBinaryGltf(testData.binary);
 
+        expect(gltf.images.Image0001.extras.source).toBeDefined();
+        expect(bufferEqual(gltf.images.Image0001.extras.source, testData.image)).toBe(true);
+    });
+
+    it('loads an embedded shader', function() {
+        var gltf = parseBinaryGltf(testData.binary);
+
+        expect(gltf.shaders.CesiumTexturedBoxTest0FS.extras.source).toBeDefined();
+        expect(bufferEqual(gltf.shaders.CesiumTexturedBoxTest0FS.extras.source, testData.shader)).toBe(true);
+    });
 });
