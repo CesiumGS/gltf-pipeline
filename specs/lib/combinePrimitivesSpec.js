1da1bce41c75fbfc20d5907d5635d9ab889b6b61
Richard Lee <agitosan@gmail.com>
2016-04-22 17:49:57 -0400

    Updated combinePrimitives stage

--- specs/lib/combinePrimitivesSpec.js
+++ specs/lib/combinePrimitivesSpec.js
@@ -1,4 +1,5 @@
 'use strict';
+var fs = require('fs');
 var addPipelineExtras = require('../../lib/addPipelineExtras');
 var combinePrimitives = require('../../lib/combinePrimitives');
 var gltfPath = './specs/data/combineObjects/combinePrimitives.gltf';
@@ -10,9 +11,15 @@ describe('addPipelineExtras', function() {
         fs.readFile(gltfPath, function(err, data) {
             gltf = JSON.parse(data);
             addPipelineExtras(gltf);
+            // console.log(JSON.stringify(gltf.meshes["Geometry-mesh019"], undefined, 2));
             done();
         });
     });
 
-    
+    it('combines two primitives referencing the same bufferView', function() {
+        combinePrimitives(gltf);
+        // console.log(JSON.stringify(gltf.meshes["Geometry-mesh019"], undefined, 2));
+        
+        fs.writeFile('combineTest.gltf', JSON.stringify(gltf, undefined, 2));
+    });
 });
