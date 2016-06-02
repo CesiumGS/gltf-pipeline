73d2526cbd0a5ceff5186d26d7039d1ecada612d
JoshuaStorm <jsbecker@princeton.edu>
2016-06-02 16:28:44 -0400

    Switched to clone instead of stringify

--- specs/lib/gltfPipelineSpec.js
+++ specs/lib/gltfPipelineSpec.js
@@ -1,9 +1,8 @@
 'use strict';
+var clone = require('clone');
 var gltfPipeline = require('../../lib/gltfPipeline');
 var readGltf = require('../../lib/readGltf');
 var writeBinaryGltf = require('../../lib/writeBinaryGltf');
-var Cesium = require('cesium');
-var defined = Cesium.defined;
 
 var gltfPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.gltf';
 var glbPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.glb';
@@ -11,10 +10,10 @@ var glbPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.glb';
 describe('gltfPipeline', function() {
     it('optimizes a gltf file', function(done) {
         readGltf(gltfPath, function(gltf) {
-            var stringCopy = JSON.stringify(gltf);
+            var gltfCopy = clone(gltf);
             gltfPipeline(gltf, function(gltf) {
                 expect(gltf).toBeDefined();
-                expect(JSON.stringify(gltf)).not.toBe(stringCopy);
+                expect(gltf).not.toBe(gltfCopy);
                 done();
             });
         });
@@ -22,10 +21,10 @@ describe('gltfPipeline', function() {
     
     it('optimizes a binary glTF file', function(done) {
         readGltf(glbPath, function(gltf) {
-            var stringCopy = JSON.stringify(gltf);
+            var gltfCopy = clone(gltf);
             gltfPipeline(gltf, function(gltf) {
                 expect(gltf).toBeDefined();
-                expect(JSON.stringify(gltf)).not.toBe(stringCopy);
+                expect(gltf).not.toBe(gltfCopy);
                 done();
             });
         });
