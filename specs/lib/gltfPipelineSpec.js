ac9e93d8e237afb901946ec811308d2eca2d70c4
JoshuaStorm <jsbecker@princeton.edu>
2016-06-03 14:59:47 -0400

    Added new tests for gltfPipelineSpec

--- specs/lib/gltfPipelineSpec.js
+++ specs/lib/gltfPipelineSpec.js
@@ -2,6 +2,7 @@
 var clone = require('clone');
 var gltfPipeline = require('../../lib/gltfPipeline');
 var processJSON = gltfPipeline.processJSON;
+var processFile = gltfPipeline.processFile;
 var readGltf = require('../../lib/readGltf');
 var writeBinaryGltf = require('../../lib/writeBinaryGltf');
 
@@ -10,24 +11,36 @@ var glbPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.glb';
 
 describe('gltfPipeline', function() {
     it('optimizes a gltf file', function(done) {
+        var gltfCopy;
         readGltf(gltfPath, function(gltf) {
-            var gltfCopy = clone(gltf);
+            gltfCopy = clone(gltf);
             processJSON(gltf, function(gltf) {
                 expect(gltf).toBeDefined();
                 expect(gltf).not.toBe(gltfCopy);
                 done();
             });
         });
+        processFile(gltfPath, function(gltf) {
+            expect(gltf).toBeDefined();
+            expect(gltf).not.toBe(gltfCopy);
+            done();
+        })
     });
     
     it('optimizes a binary glTF file', function(done) {
+        var gltfCopy;
         readGltf(glbPath, function(gltf) {
-            var gltfCopy = clone(gltf);
+            gltfCopy = clone(gltf);
             processJSON(gltf, function(gltf) {
                 expect(gltf).toBeDefined();
                 expect(gltf).not.toBe(gltfCopy);
                 done();
             });
+        });
+        processFile(glbPath, function(gltf) {
+            expect(gltf).toBeDefined();
+            expect(gltf).not.toBe(gltfCopy);
+            done();
         });
     });
 });
