e045dd32194f3829deef57861dea3013dfbdf5b5
Robert Taglang <minitags@comcast.net>
2016-06-07 16:50:30 -0400

    Added quantized attribute tests

--- specs/lib/quantizeAttributesSpec.js
+++ specs/lib/quantizeAttributesSpec.js
@@ -1,38 +1,141 @@
 'use strict';
+var clone = require('clone');
 
-var fs = require('fs');
+var byteLengthForComponentType = require('../../lib/byteLengthForComponentType');
+var numberOfComponentsForType = require('../../lib/numberOfComponentsForType');
 var quantizeAttributes = require('../../lib/quantizeAttributes');
-var addPipelineExtras = require('../../lib/addPipelineExtras');
-var loadGltfUris = require('../../lib/loadGltfUris');
-var writeGltf = require('../../lib/writeGltf')
-var boxPath = './specs/data/quantized/';
-var outputPath = './output/Duck-Quantized.gltf';
 
 describe('quantizeAttributes', function() {
-    var gltf;
-
-    beforeAll(function(done) {
-        fs.readFile(boxPath + 'Duck.gltf', function(err, data) {
-            if(!err) {
-                gltf = JSON.parse(data);
-                addPipelineExtras(gltf);
-                loadGltfUris(gltf, boxPath, function() {
-                    done();
-                });
-            }
-        });
-    });
-
-    it('quantizes positions', function() {
-        quantizeAttributes(gltf, {
-            "attributes": {
-                "LOD3spShape-lib": {
-                    "0": [
-                        "POSITION"
-                    ]
+    var buffer = new Buffer(new Uint8Array(108));
+    var testGltf = {
+        accessors : {
+            // Interleaved accessors in bufferView_0
+            accessor_0 : {
+                bufferView : 'bufferView_0',
+                byteOffset : 0,
+                byteStride : 18,
+                componentType : 5126,
+                count : 3,
+                min : [-1.0, -1.0, -1.0],
+                max : [1.0, 1.0, 1.0],
+                type : 'VEC3',
+            },
+            accessor_1 : {
+                bufferView : 'bufferView_0',
+                byteOffset : 12,
+                byteStride : 18,
+                componentType : 5123,
+                count : 3,
+                min : [-1.0, -1.0, -1.0],
+                max : [1.0, 1.0, 1.0],
+                type : 'VEC2'
+            },
+            // Block accessors in bufferView_1
+            accessor_2 : {
+                bufferView : 'bufferView_1',
+                byteOffset : 0,
+                byteStride : 12,
+                componentType : 5126,
+                count : 3,
+                min : [-1.0, -1.0, -1.0],
+                max : [1.0, 1.0, 1.0],
+                type : 'VEC3'
+            },
+            // Already quantized
+            accessor_3 : {
+                bufferView : 'bufferView_1',
+                byteOffset : 36,
+                componentType : 5126,
+                count : 3,
+                min : [0, 0, 0],
+                max : [65535, 65535, 65535],
+                type : 'VEC2',
+                extensions : {
+                    WEB3D_quantized_attributes : {
+                        decodeMatrix : [
+                            1.0, 0.0, 0.0,
+                            0.0, 1.0, 0.0,
+                            0.0, 0.0, 1.0
+                        ],
+                        decodeMin : [-1.0, -1.0],
+                        decodeMax : [1.0, 1.0]
+                    }
                 }
             }
-        });
-        writeGltf(gltf, outputPath, true);
+        },
+        bufferViews : {
+            bufferView_0 : {
+                buffer : 'buffer',
+                byteLength : 48,
+                byteOffset : 0,
+                target : 34962
+            },
+            bufferView_1 : {
+                buffer : 'buffer',
+                byteLength : 60,
+                byteOffset : 48,
+                target : 34962
+            }
+        },
+        buffers : {
+            buffer : {
+                byteLength : buffer.length,
+                type : 'arraybuffer',
+                extras : {
+                    _pipeline : {}
+                }
+            }
+        },
+        meshes : {
+            mesh : {
+                primitives : [
+                    {
+                        attributes : {
+                            POSITION : 'accessor_0',
+                            NORMAL : 'accessor_1'
+                        }
+                    },
+                    {
+                        attributes : {
+                            POSITION : 'accessor_2',
+                            TEXCOORD : 'accessor_3'
+                        }
+                    }
+                ]
+            }
+        }
+    };
+
+    it('Doesn\'t quantize if options.semantics is empty', function() {
+        var gltf = clone(testGltf);
+        gltf.buffers.buffer.extras._pipeline.source = buffer;
+        quantizeAttributes(gltf, {semantics: []});
+        expect(gltf.buffers.buffer.byteLength).toEqual(buffer.length);
+    });
+
+    it('Quantizes attributes for semantic', function() {
+        var gltf = clone(testGltf);
+        var accessor_0 = gltf.accessors.accessor_0;
+        var accessor_2 = gltf.accessors.accessor_2;
+        var size = byteLengthForComponentType(accessor_0.componentType) * numberOfComponentsForType(accessor_0.type) * accessor_0.count;
+        size += byteLengthForComponentType(accessor_2.componentType) * numberOfComponentsForType(accessor_2.type) * accessor_2.count;
+        size = size/2.0;
+        gltf.buffers.buffer.extras._pipeline.source = buffer;
+        quantizeAttributes(gltf, {semantics: ['POSITION']});
+        expect(gltf.buffers.buffer.byteLength + size).toEqual(buffer.length);
+    });
+
+    it('Doesn\'t quantize non-float attribute', function() {
+        var gltf = clone(testGltf);
+        gltf.buffers.buffer.extras._pipeline.source = buffer;
+        quantizeAttributes(gltf, {semantics: ['NORMAL']});
+        expect(gltf.buffers.buffer.byteLength).toEqual(buffer.length);
+    });
+
+    it('Doesn\'t quantize already quantized attribute', function() {
+        var gltf = clone(testGltf);
+        gltf.buffers.buffer.extras._pipeline.source = buffer;
+        quantizeAttributes(gltf, {semantics: ['TEXCOORD']});
+        expect(gltf.buffers.buffer.byteLength).toEqual(buffer.length);
     });
 });
