30a5565f26c8f5faaec020f34769b228338e171d
Sean Lilley <lilleyse@gmail.com>
2016-06-09 10:49:33 -0400

    Fix

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = {
     gltfPipeline : require('./lib/gltfPipeline'),
     readGltf : require('./lib/readGltf'),
     writeBinaryGltf : require('./lib/writeBinaryGltf'),
-    writeGltf : require('./lib/writeGltf'),
+    writeGltf : require('./lib/writeGltf')
 };
