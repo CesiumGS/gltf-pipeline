984725241cc2d34829a738432405c7b79f182809
JoshuaStorm <jsbecker@princeton.edu>
2016-06-09 15:16:23 -0400

    Added '--name' style flags

--- bin/gltf-pipeline.js
+++ bin/gltf-pipeline.js
@@ -15,24 +15,24 @@ var readGltf = require('../lib/readGltf');
 if (process.argv.length < 3 || defined(argv.h) || defined(argv.help)) {
     var help =
         'Usage: node ' + path.basename(__filename) + ' [path-to.gltf or path-to.bgltf] [OPTIONS]\n' +
-        '  -i, input=PATH Read unoptimized glTF from the specified file.\n' +
-        '  -o, output=PATH write optimized glTF to the specified file.\n' +
-        '  -b, write binary glTF file.\n' +
-        '  -s, writes out separate geometry/animation data files, shader files and textures instead of embedding them in the glTF file.\n' +
-        '  --separateImage, write out separate textures, but embed geometry/animation data files, and shader files.\n';
+        '  -i --input, input=PATH Read unoptimized glTF from the specified file.\n' +
+        '  -o --output, output=PATH write optimized glTF to the specified file.\n' +
+        '  -b --binary, write binary glTF file.\n' +
+        '  -s --separate, writes out separate geometry/animation data files, shader files and textures instead of embedding them in the glTF file.\n' +
+        '  -t --separateTexture, write out separate textures, but embed geometry/animation data files, and shader files.\n';
     process.stdout.write(help);
     return;
 }
 
-var gltfPath = defaultValue(argv._[0], argv.i);
+var gltfPath = defaultValue(argv._[0], argv.i || argv.input);
 var fileExtension = path.extname(gltfPath);
 var fileName = path.basename(gltfPath, fileExtension);
 var filePath = path.dirname(gltfPath);
 
-var outputPath = defaultValue(argv._[1], argv.o);
-var isSeparate = defaultValue(argv.s, false);
-var isBinary = defaultValue(argv.b, false);
-var isImageSeparate = defaultValue(argv.separateImage, false);
+var outputPath = defaultValue(argv._[1], argv.o || argv.output);
+var isBinary = defaultValue(argv.b || argv.binary, false);
+var isSeparate = defaultValue(argv.s || argv.separate, false);
+var isTextureSeparate = defaultValue(argv.t || argv.separateTexture, false);
 
 if (!defined(gltfPath)) {
     throw new DeveloperError('Input path is undefined.');
@@ -50,7 +50,7 @@ if (!defined(outputPath)) {
 var options = {
     isBinary : isBinary,
     isEmbedded : !isSeparate,
-    isImageEmbedded : !isImageSeparate
+    isImageEmbedded : !isTextureSeparate
 };
 
 processFileToDisk(gltfPath, outputPath, options);
