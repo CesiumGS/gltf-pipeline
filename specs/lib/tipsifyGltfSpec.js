7481951d48eccf144caa5dd2bef7bd49b8a53abe
JoshuaStorm <jsbecker@princeton.edu>
2016-06-10 17:51:58 -0400

    Changed spec test

--- specs/lib/tipsifyGltfSpec.js
+++ specs/lib/tipsifyGltfSpec.js
@@ -16,47 +16,8 @@ describe('tipsifyGltf', function() {
             var copyGltf = clone(gltf);
             tipsifyGltf(gltf);
             
-            var indices = [];
-            var accessors = gltf.accessors;
-            var meshes = gltf.meshes;
-            for (var meshId in meshes) {
-                if (meshes.hasOwnProperty(meshId)) {
-                    var mesh = meshes[meshId];
-                    var primitives = mesh.primitives;
-                    var primitivesLength = primitives.length;
-                    for (var i = 0; i < primitivesLength; i++) {
-                        var primitive = primitives[i];
-                        var indexAccessorId = primitive.indices;
-                        var indexAccessor = accessors[indexAccessorId];
-
-                        var indicesArray = readAccessor(indexAccessor, gltf);
-                        indices.push(indicesArray);
-                    }
-                }
-            }
-            var copyIndices = [];
-            var copyAccessors = copyGltf.accessors;
-            var copyMeshes = copyGltf.meshes;
-            for (var copyMeshId in copyMeshes) {
-                if (copyMeshes.hasOwnProperty(copyMeshId)) {
-                    var copyMesh = copyMeshes[copyMeshId];
-                    var copyPrimitives = copyMesh.primitives;
-                    var copyPrimitivesLength = copyPrimitives.length;
-                    for (i = 0; i < copyPrimitivesLength; i++) {
-                        var copyPrimitive = copyPrimitives[i];
-                        var copyIndexAccessorId = copyPrimitive.indices;
-                        var copyIndexAccessor = copyAccessors[indexAccessorId];
-
-                        var copyIndicesArray = readAccessor(indexAccessor, gltf);
-                        copyIndices.push(copyIndicesArray);
-
-                    }
-                }
-            }
-            expect(indices).not.toEqual(copyIndices);
+            expect(gltf).not.toEqual(copyGltf);
             done();
-            
         });
-        
     });
 });
