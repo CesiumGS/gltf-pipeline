1ffae9166ffccb5edef7c706063eecd91bdd482b
Kangning Li <likangning93@gmail.com>
2016-06-10 16:28:44 -0400

    changed spec to use readGltf

--- specs/lib/readAccessorSpec.js
+++ specs/lib/readAccessorSpec.js
@@ -12,6 +12,7 @@ var clone = require('clone');
 var loadGltfUris = require('../../lib/loadGltfUris');
 var addPipelineExtras = require('../../lib/addPipelineExtras');
 var readAccessor = require('../../lib/readAccessor');
+var readGltf = require('../../lib/readGltf');
 
 var gltfPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.gltf';
 
@@ -19,20 +20,9 @@ describe('bakeAmbientOcclusion', function() {
     var boxGltf;
 
     beforeAll(function(done) {
-        fs.readFile(gltfPath, function(err, data) {
-            if (err) {
-                throw err;
-            }
-            else {
-                boxGltf = JSON.parse(data);
-                addPipelineExtras(boxGltf);
-                loadGltfUris(boxGltf, path.dirname(gltfPath), function(err, gltf) {
-                    if (err) {
-                        throw err;
-                    }
-                    done();
-                });
-            }
+        readGltf(gltfPath, function(gltf) {
+            boxGltf = gltf;
+            done();
         });
     });
 
