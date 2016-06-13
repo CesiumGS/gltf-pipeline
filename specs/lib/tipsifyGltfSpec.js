72b0e84d83d07cc277372f03ec24b06b2d1f6420
JoshuaStorm <jsbecker@princeton.edu>
2016-06-13 10:20:51 -0400

    Update for updated readAccessor function

--- specs/lib/tipsifyGltfSpec.js
+++ specs/lib/tipsifyGltfSpec.js
@@ -1,5 +1,6 @@
 'use strict';
 var clone = require('clone');
+var diff = require('diff');
 var tipsifyGltf = require('../../lib/tipsifyGltf');
 var addDefaults = require('../../lib/addDefaults');
 var readAccessor = require('../../lib/readAccessor');
@@ -8,14 +9,12 @@ var readGltf = require('../../lib/readgltf');
 var gltfPath = './specs/data/tipsifyGltf/Cesium_Ground.gltf';
 
 describe('tipsifyGltf', function() {
-    
-
     it('reorders indices', function(done) {
         readGltf(gltfPath, function(gltf) {
             addDefaults(gltf);
             var copyGltf = clone(gltf);
             tipsifyGltf(gltf);
-            
+
             expect(gltf).not.toEqual(copyGltf);
             done();
         });
