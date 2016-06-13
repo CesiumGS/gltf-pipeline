4c1593774ef3701e08ccf118f5c63ed144010fcd
JoshuaStorm <jsbecker@princeton.edu>
2016-06-13 17:31:26 -0400

    writeAccessor

--- specs/lib/tipsifyGltfSpec.js
+++ specs/lib/tipsifyGltfSpec.js
@@ -4,7 +4,7 @@ var tipsifyGltf = require('../../lib/tipsifyGltf');
 var addDefaults = require('../../lib/addDefaults');
 var readGltf = require('../../lib/readgltf');
 
-var gltfPath = './specs/data/tipsifyGltf/Cesium_Ground.gltf';
+var gltfPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.gltf';
 
 describe('tipsifyGltf', function() {
     it('reorders indices', function(done) {
@@ -12,8 +12,8 @@ describe('tipsifyGltf', function() {
             addDefaults(gltf);
             var copyGltf = clone(gltf);
             tipsifyGltf(gltf);
-
-            expect(gltf).not.toEqual(copyGltf);
+            
+            expect(clone(gltf)).not.toEqual(copyGltf);
             done();
         });
     });
