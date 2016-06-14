8ae0f4efbf3c62e341d6d75d412bbd26140daeb3
JoshuaStorm <jsbecker@princeton.edu>
2016-06-14 13:14:47 -0400

    Addressed comments

--- specs/lib/tipsifyGltfSpec.js
+++ specs/lib/tipsifyGltfSpec.js
@@ -1,20 +1,19 @@
 'use strict';
-var tipsifyGltf = require('../../lib/tipsifyGltf');
+var cacheOptimizeIndices = require('../../lib/cacheOptimizeIndices');
 var addDefaults = require('../../lib/addDefaults');
 var readGltf = require('../../lib/readGltf');
-var writeAccessor = require('../../lib/writeAccessor');
 var readAccessor = require('../../lib/readAccessor');
 
 var gltfPath = './specs/data/tipsifyGltf/chinesedragon.gltf';
 
-describe('tipsifyGltf', function() {
+describe('cacheOptimizeIndices', function() {
     it('reorders indices', function(done) {
         readGltf(gltfPath, function(gltf) {
             addDefaults(gltf);
-            var indexAccessorId = gltf.meshes[Object.keys(gltf.meshes)].primitives[0].indices;
+            var indexAccessorId = gltf.meshes[Object.keys(gltf.meshes)[0]].primitives[0].indices;
             var indicesBefore = readAccessor(gltf, gltf.accessors[indexAccessorId]);
             
-            tipsifyGltf(gltf);
+            cacheOptimizeIndices(gltf);
             
             var indicesAfter = readAccessor(gltf, gltf.accessors[indexAccessorId]);
             expect(indicesBefore).not.toEqual(indicesAfter);
