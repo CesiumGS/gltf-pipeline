74d5355e6346aaa99cf6d1d8f3174727ad54b0d9
JoshuaStorm <jsbecker@princeton.edu>
2016-06-15 14:43:32 -0400

    Fix typo

--- specs/lib/cacheOptimizeIndicesSpec.js
+++ specs/lib/cacheOptimizeIndicesSpec.js
@@ -5,12 +5,14 @@ var readGltf = require('../../lib/readGltf');
 var readAccessor = require('../../lib/readAccessor');
 var writeAccessor = require('../../lib/writeAccessor');
 
-var gltfPath = './specs/data/boxTexturedUnOptimized/CesiumTexturedBoxTest.gltf';
+var gltfPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.gltf';
 var optimizedIndices = [ 0, 1, 2, 3, 2, 1, 4, 5, 6, 7, 6, 5, 8, 9, 10, 11, 10, 9, 12, 
     13, 14, 15, 14, 13, 16, 17, 18, 19, 18, 17, 20, 21, 22, 23, 22, 21 ];
 
 describe('cacheOptimizeIndices', function() {
     it('reorders indices', function(done) {
+
+
         readGltf(gltfPath, function(gltf) {
             addDefaults(gltf);
             var indexAccessorId = gltf.meshes[Object.keys(gltf.meshes)[0]].primitives[0].indices;
