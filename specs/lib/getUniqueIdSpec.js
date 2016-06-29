62b96d2c750648639734164a8568aa77b8f44345
Kangning Li <likangning93@gmail.com>
2016-06-29 16:00:55 -0400

    PR118: changed getUniqueId to only look at top level gltf objects that have ids.

--- specs/lib/getUniqueIdSpec.js
+++ specs/lib/getUniqueIdSpec.js
@@ -12,64 +12,73 @@ describe('getUniqueId', function() {
             },
             "accessor_1": {
                 "bufferView": "bufferView_138"
-            },
-            "accessor_2": {
-                "bufferView": "bufferView_138"
-            },
-            "accessor_3": {
-                "bufferView": "bufferView_138"
-            },
-            "name": {
-                "bufferView": "bufferView_138"
             }
         },
         "buffers": {
             "bufferId": {
-                "byteLength": 840,
-                "uri": "buffer.bin"
-            },
-            "name_0": {
-                "byteLength": 840,
-                "uri": "buffer.bin"
+                "access": ''
             }
         },
         "materials": {
-            "blinn-1": {
-                "technique": "technique1",
-                "values": {
-                    "ambient": [0, 0, 0, 1]
-                },
-                "name": "blinn1"
-            },
-            "lambert-1": {
-                "technique": "technique1",
-                "values": {
-                    "ambient": [0, 0, 0, 1],
-                    "diffuse": "texture_file2"
-                },
-                "name": "lambert1"
-            },
-            "name_1": {
-                "technique": "technique1",
-                "values": {
-                    "ambient": [0, 0, 0, 1],
-                    "diffuse": "texture_file2",
-                    "name_2": "wrongName"
-                },
-                "name": "lambert1"
-            }
+            "blinn-1": {},
+            "lambert-1": {}
+        }
+    };
+
+    var allIdentifiersGltf = {
+        "nodes": {
+            "name" : ''
+        },
+        "skins": {
+            "name_0": ''
+        },
+        "cameras": {
+            "name_1": ''
+        },
+        "meshes": {
+            "name_2": ''
+        },
+        "accessors": {
+            "name_3": ''
+        },
+        "materials": {
+            "name_4": ''
+        },
+        "bufferViews": {
+            "name_5": ''
+        },
+        "techniques": {
+            "name_6": ''
+        },
+        "textures": {
+            "name_7": ''
+        },
+        "buffers": {
+            "name_8": ''
+        },
+        "programs": {
+            "name_9": ''
+        },
+        "images": {
+            "name_10": ''
+        },
+        "samplers": {
+            "name_11": ''
+        },
+        "shaders": {
+            "name_12": ''
         }
     };
 
     it('locates IDs in the top level objects of the gltf and adjusts accordingly', function() {
-        expect(getUniqueId(gltf, 'accessor')).toEqual('accessor_4');
+        expect(getUniqueId(gltf, 'accessor')).toEqual('accessor_2');
     });
 
     it('does not modify the prefix if the prefix does not occur in top level objects of the gltf', function() {
         expect(getUniqueId(gltf, 'access')).toEqual('access');
     });
 
-    it('can identify instances of the prefix across all top level objects', function() {
-        expect(getUniqueId(gltf, 'name')).toEqual('name_2');
+    it('can identify instances of the prefix across different top level objects', function() {
+        expect(getUniqueId(allIdentifiersGltf, 'name')).toEqual('name_13');
     });
 });
