7df552d85249a7c39acec44cb1d7b8c1b7f50907
Robert Taglang <minitags@comcast.net>
2016-08-12 17:34:27 -0400

    Added some test skeletons

--- specs/lib/combinePrimitivesSpec.js
+++ specs/lib/combinePrimitivesSpec.js
@@ -6,32 +6,34 @@ var removePipelineExtras = require('../../lib/removePipelineExtras');
 var doubleBoxToCombinePath = './specs/data/combineObjects/doubleBoxToCombine.gltf';
 
 describe('combinePrimitives', function() {
-    var options = {};
+    var arrayOneA = new Float32Array([1, 2, 3]);
+    var bufferOneA = new Buffer(arrayOneA.buffer);
+    var arrayTwoA = new Float32Array([4, 5, 6]);
+    var bufferTwoA = new Buffer(arrayTwoA.buffer);
+    var arrayOneB = new Uint16Array([1, 2, 3]);
+    var bufferOneB = new Buffer(arrayOneB.buffer);
+    var arrayTwoB = new Uint16Array([4, 5, 6]);
+    var bufferTwoB = new Buffer(arrayTwoB.buffer);
 
-    it('combines two primitives', function(done) {
-        expect(readGltf(doubleBoxToCombinePath, options)
-            .then(function(gltf) {
-                var accessors = gltf.accessors;
-                var doubleBoxToCombine = gltf;
-                combinePrimitives(doubleBoxToCombine);
+    it('combines two primitives by concatenating them', function() {
 
-                var primitives = doubleBoxToCombine.meshes.meshTest.primitives;
-                expect(primitives.length).toEqual(1);
-                var primitive = primitives[0];
-                expect(primitive.material).toEqual('Effect_outer');
-                expect(primitive.mode).toEqual(4);
-
-                var indexAccessor = accessors[primitive.indices];
-                expect(indexAccessor.max).toEqual([319]);
-                expect(indexAccessor.min).toEqual([0]);
-
-                var positionAccessor = accessors[primitive.attributes.POSITION];
-                expect(positionAccessor.max).toEqual([0.5, 0.5, 0.5]);
-                expect(positionAccessor.min).toEqual([-0.5, -0.5, -0.5]);
-
-                var normalAccessor = accessors[primitive.attributes.NORMAL];
-                expect(normalAccessor.max).toEqual([1.0, 1.0, 1.0]);
-                expect(normalAccessor.min).toEqual([-1.0, -1.0, -1.0]);
-            }), done).toResolve();
     });
+
+    it('combines two primitives with shared attribute accessors by merging them', function() {
+
+    });
+
+    it('combines three primitives, merging two and then concatenating the result with the third', function() {
+
+    });
+
+    it('doesn\'t combine primitive that has attribute accessors that are different sizes', function() {
+
+    });
+
+    it('doesn\'t combine primitives that share a single attribute accessor', function() {
+
+    });
+
+    it('doesn\'t combine primitives that )
 });
