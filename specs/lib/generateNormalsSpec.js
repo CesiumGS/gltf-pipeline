702319acdae037e58033279cde77f3a2f31d052c
Robert Taglang <minitags@comcast.net>
2016-09-30 13:22:06 -0400

    Makes hard normal generation more generic

--- specs/lib/generateNormalsSpec.js
+++ specs/lib/generateNormalsSpec.js
@@ -8,26 +8,53 @@ var gltfNoNormalsPath = './specs/data/generateNormals/box_no_normals.gltf';
 var gltfNormalsPath = './specs/data/generateNormals/box_normals.gltf';
 
 describe('generateNormals', function(){
-    it('generates normals if they do not exist', function(done) {
-        expect(readGltf(gltfNoNormalsPath)
-            .then(function(gltf){
-                var byteLengthBefore = 168;
-                generateNormals(gltf);
-            
-                var attributes = gltf.meshes.mesh_box.primitives[0].attributes;
-                var byteLengthAfter = gltf.buffers[Object.keys(gltf.buffers)[0]].byteLength;
-                expect(attributes.NORMAL).toBeDefined();
-                expect(gltf.accessors[attributes.NORMAL]).toBeDefined();
-                expect(byteLengthAfter).toBe(byteLengthBefore + 8 * 3 * 4); // 8 normals are generated
-            }), done).toResolve();
+    var gltfNoNormals;
+    var gltfNormals;
+
+    beforeAll(function(done) {
+        readGltf(gltfNoNormalsPath)
+            .then(function(gltf) {
+                gltfNoNormals = gltf;
+                return readGltf(gltfNormalsPath);
+            })
+            .then(function(gltf) {
+                gltfNormals = gltf;
+                done();
+            });
     });
 
-    it('does not generate normals if they already exist', function(done) {
-        expect(readGltf(gltfNormalsPath)
-            .then(function(gltf) {
-                var gltfCopy = clone(gltf);
-                generateNormals(gltf);
-                expect(clone(gltf)).toEqual(gltfCopy);
-            }), done).toResolve();
+    it('generates normals if they do not exist', function() {
+        var gltf = gltfNoNormals;
+        var byteLengthBefore = 168;
+        generateNormals(gltf);
+
+        var attributes = gltf.meshes.mesh_box.primitives[0].attributes;
+        var byteLengthAfter = gltf.buffers[Object.keys(gltf.buffers)[0]].byteLength;
+        expect(attributes.NORMAL).toBeDefined();
+        expect(gltf.accessors[attributes.NORMAL]).toBeDefined();
+        expect(byteLengthAfter).toBe(byteLengthBefore + 8 * 3 * 4); // 8 normals are generated
     });
+
+    it('does not generate normals if they already exist', function() {
+        var gltf = gltfNormals;
+        var gltfCopy = clone(gltf);
+        generateNormals(gltf);
+        expect(gltf.meshes).toEqual(gltfCopy.meshes);
+    });
+
+    it('generates hard normals if they do not exist', function() {
+        var gltf = gltfNoNormals;
+        var byteLengthBefore = 168;
+        generateNormals(gltf, {
+            hardNormals : true
+        });
+
+        var attributes = gltf.meshes.mesh_box.primitives[0].attributes;
+        var byteLengthAfter = gltf.buffers[Object.keys(gltf.buffers)[0]].byteLength;
+        expect(attributes.NORMAL).toBeDefined();
+        expect(gltf.accessors[attributes.NORMAL]).toBeDefined();
+        expect(byteLengthAfter).toBe(byteLengthBefore + 8 * 3 * 4); // 8 normals are generated
+    });
+
+
 });
