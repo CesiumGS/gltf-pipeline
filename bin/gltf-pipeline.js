934c5c05c9defaa596f56157af69aef7f2b47971
Robert Taglang <minitags@comcast.net>
2016-10-14 09:09:09 -0400

    Finalized hard normal support and added stage for removing normals

--- bin/gltf-pipeline.js
+++ bin/gltf-pipeline.js
@@ -22,6 +22,7 @@ if (process.argv.length < 3 || defined(argv.h) || defined(argv.help)) {
         '  -q --quantize, quantize the attributes of this model.\n' +
         '  -n --encodeNormals, oct-encode the normals of this model.\n' +
         '  -c --compressTextureCoordinates, compress the texture coordinates of this model.\n' +
+        '  -r --removeNormals, strips off existing normals, allowing them to be regenerated.\n' +
         '  -f --faceNormals, if normals are missing, they should be generated using the face normal\n' +
         '     --ao: Bake ambient occlusion to vertex data using default settings ONLY. When specifying other settings, do not use `--ao` on its own. Default: inactive.\n' +
         '     --ao.toTexture: Bake AO to existing diffuse textures instead of to vertices. Does not modify shaders. Default: inactive.\n' +
@@ -41,6 +42,7 @@ var separateImage = defaultValue(defaultValue(argv.t, argv.separateImage), false
 var quantize = defaultValue(defaultValue(argv.q, argv.quantize), false);
 var encodeNormals = defaultValue(defaultValue(argv.n, argv.encodeNormals), false);
 var compressTextureCoordinates = defaultValue(defaultValue(argv.c, argv.compressTextureCoordinates), false);
+var removeNormals = defaultValue(defaultValue(argv.r, argv.removeNormals), false);
 var faceNormals = defaultValue(defaultValue(argv.f, argv.faceNormals), false);
 var aoOptions = argv.ao;
 var typeofAoOptions = typeof(aoOptions);
@@ -70,6 +72,7 @@ var options = {
     embed : !separate,
     embedImage : !separateImage,
     encodeNormals : encodeNormals,
+    removeNormals : removeNormals,
     faceNormals : faceNormals,
     optimizeForCesium : optimizeForCesium,
     quantize : quantize
