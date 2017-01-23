a4cb0bca4d22eb27e591152288001e18d518cd3c
Shehzan Mohammed <zmohammed@agi.com>
2017-01-23 14:55:37 -0500

    Typographics corrections and minor edits in computeStatistics and spec

--- specs/lib/computeStatisticsSpec.js
+++ specs/lib/computeStatisticsSpec.js
@@ -6,42 +6,41 @@ var getStatistics = require('../../lib/computeStatistics').getStatistics;
 var readGltf = require('../../lib/readGltf');
 
 describe('computeStatistics', function() {
-
     it('should return stats for simple box test', function(done){
         expect(readGltf('specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.gltf')
-                .then(function(gltf) {
-                    var stats = getStatistics(gltf);
+            .then(function(gltf) {
+                var stats = getStatistics(gltf);
 
-                    expect(stats.buffersSizeInBytes).toEqual(840);
-                    expect(stats.numberOfImages).toEqual(1);
-                    expect(stats.numberOfExternalRequests).toEqual(4);
+                expect(stats.buffersSizeInBytes).toEqual(840);
+                expect(stats.numberOfImages).toEqual(1);
+                expect(stats.numberOfExternalRequests).toEqual(4);
 
-                    expect(stats.numberOfDrawCalls).toEqual(1);
-                    expect(stats.numberOfRenderedPrimitives).toEqual(0);
+                expect(stats.numberOfDrawCalls).toEqual(1);
+                expect(stats.numberOfRenderedPrimitives).toEqual(0);
 
-                    expect(stats.numberOfNodes).toEqual(4);
-                    expect(stats.numberOfMeshes).toEqual(1);
-                    expect(stats.numberOfMaterials).toEqual(1);
-                    expect(stats.numberOfAnimations).toEqual(0);
-                }), done).toResolve();
+                expect(stats.numberOfNodes).toEqual(4);
+                expect(stats.numberOfMeshes).toEqual(1);
+                expect(stats.numberOfMaterials).toEqual(1);
+                expect(stats.numberOfAnimations).toEqual(0);
+            }), done).toResolve();
     });
 
     it('works with rigged test', function(done) {
         expect(readGltf('specs/data/riggedSimpleUnoptimized/riggedSimple.gltf')
-                .then(function(gltf) {
-                    var stats = getStatistics(addDefaults(gltf));
+            .then(function(gltf) {
+                var stats = getStatistics(addDefaults(gltf));
 
-                    expect(stats.buffersSizeInBytes).toEqual(10468);
-                    expect(stats.numberOfImages).toEqual(0);
-                    expect(stats.numberOfExternalRequests).toEqual(3);
+                expect(stats.buffersSizeInBytes).toEqual(10468);
+                expect(stats.numberOfImages).toEqual(0);
+                expect(stats.numberOfExternalRequests).toEqual(3);
 
-                    expect(stats.numberOfDrawCalls).toEqual(1);
-                    expect(stats.numberOfRenderedPrimitives).toEqual(0);
+                expect(stats.numberOfDrawCalls).toEqual(1);
+                expect(stats.numberOfRenderedPrimitives).toEqual(0);
 
-                    expect(stats.numberOfNodes).toEqual(5);
-                    expect(stats.numberOfMeshes).toEqual(1);
-                    expect(stats.numberOfMaterials).toEqual(1);
-                    expect(stats.numberOfAnimations).toEqual(2);
-                }), done).toResolve();
+                expect(stats.numberOfNodes).toEqual(5);
+                expect(stats.numberOfMeshes).toEqual(1);
+                expect(stats.numberOfMaterials).toEqual(1);
+                expect(stats.numberOfAnimations).toEqual(2);
+            }), done).toResolve();
     });
 });
