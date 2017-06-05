df947192ddfd7ab024c48de25ae6eec577a62e9d
Sean Lilley <lilleyse@gmail.com>
2017-06-05 15:20:29 -0400

    Fix remove normals

--- specs/lib/removeNormalsSpec.js
+++ specs/lib/removeNormalsSpec.js
@@ -1,16 +1,19 @@
 'use strict';
 
 var removeNormals = require('../../lib/removeNormals');
+var techniqueParameterForSemantic = require('../../lib/techniqueParameterForSemantic');
 
 describe('removeNormals', function() {
-    it('removes normals and strips the technique from the material', function() {
+    it('removes normals and generates constant technique', function() {
         var gltf = {
+            asset : {},
             meshes : {
                 mesh: {
                     primitives: [
                         {
                             attributes: {
-                                NORMAL: 'some_accessor'
+                                POSITION : 'position_accessor',
+                                NORMAL: 'normal_accessor'
                             },
                             material: 'material'
                         }
@@ -27,12 +30,18 @@ describe('removeNormals', function() {
             }
         };
         removeNormals(gltf);
-        expect(gltf.meshes.mesh.primitives[0].attributes.NORMAL).not.toBeDefined();
-        expect(gltf.materials.material.technique).not.toBeDefined();
+        var primitive = gltf.meshes.mesh.primitives[0];
+        var material = gltf.materials[primitive.material];
+        var technique = gltf.techniques[material.technique];
+        expect(primitive.attributes.NORMAL).toBeUndefined();
+        expect(technique).toBeDefined();
+        var parameter = techniqueParameterForSemantic(technique, 'NORMAL');
+        expect(parameter).toBeUndefined(); // Technique should not reference normals
     });
 
     it('handles undefined technique', function() {
         var gltf = {
+            asset : {},
             meshes : {
                 mesh: {
                     primitives: [
@@ -50,11 +59,12 @@ describe('removeNormals', function() {
             }
         };
         removeNormals(gltf);
-        expect(gltf.meshes.mesh.primitives[0].attributes.NORMAL).not.toBeDefined();
+        expect(gltf.meshes.mesh.primitives[0].attributes.NORMAL).toBeUndefined();
     });
 
     it('handles undefined material', function() {
         var gltf = {
+            asset : {},
             meshes : {
                 mesh: {
                     primitives: [
@@ -68,11 +78,12 @@ describe('removeNormals', function() {
             }
         };
         removeNormals(gltf);
-        expect(gltf.meshes.mesh.primitives[0].attributes.NORMAL).not.toBeDefined();
+        expect(gltf.meshes.mesh.primitives[0].attributes.NORMAL).toBeUndefined();
     });
 
     it('only deletes normals and handles subscripts', function() {
         var gltf = {
+            asset : {},
             meshes : {
                 mesh: {
                     primitives: [{
@@ -100,19 +111,16 @@ describe('removeNormals', function() {
                 }
             },
             techniques : {
-                technique : {}
+                technique : {},
+                technique2 : {}
             }
         };
         removeNormals(gltf);
         var primitives = gltf.meshes.mesh.primitives;
-        expect(primitives[0].attributes.NORMAL).not.toBeDefined();
+        expect(primitives[0].attributes.NORMAL).toBeUndefined();
         expect(primitives[0].attributes.POSITION).toBeDefined();
-        expect(primitives[0].material).toBeDefined();
-        expect(gltf.materials.material.technique).not.toBeDefined();
-        expect(primitives[1].attributes.NORMAL).not.toBeDefined();
-        expect(primitives[1].attributes.NORMAL_0).not.toBeDefined();
+        expect(primitives[1].attributes.NORMAL).toBeUndefined();
+        expect(primitives[1].attributes.NORMAL_0).toBeUndefined();
         expect(primitives[1].attributes.POSITION).toBeDefined();
-        expect(primitives[1].material).toBeDefined();
-        expect(gltf.materials.material2.technique).not.toBeDefined();
     });
 });
