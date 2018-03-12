5e896d5581248cd32d594ccd34a7a26a23a682ae
Sean Lilley <lilleyse@gmail.com>
2018-03-12 19:03:14 -0400

    Added test

--- specs/lib/processModelMaterialsCommonSpec.js
+++ specs/lib/processModelMaterialsCommonSpec.js
@@ -26,6 +26,14 @@ describe('processModelMaterialsCommon', function() {
                             technique : 'BLINN'
                         }
                     }
+                },
+                {
+                    // Second unused material included for testing purposes
+                    extensions : {
+                        KHR_materials_common : {
+                            technique : 'BLINN'
+                        }
+                    }
                 }
             ],
             nodes: [
