53c52f728d9f27100d1d5f11abb141e6711804b1
Sean Lilley <lilleyse@gmail.com>
2018-04-23 21:55:25 -0400

    Finished tests for writeResources

--- specs/lib/writeResourcesSpec.js
+++ specs/lib/writeResourcesSpec.js
@@ -9,16 +9,10 @@ var writeResources = require('../../lib/writeResources');
 
 var gltfPath = 'specs/data/2.0/box-textured-embedded/box-textured-embedded.gltf';
 var gltf;
-// separateBuffer writes separate buffers (and nothing else)
-// separateShaders writes separate shaders (and nothing else)
-// separateTextures writes separate textures (and nothing else)
-// dataUri is used instead of bfferviews
-// bufferstorage supplied,
-// external resources supplied (part of above really)
 
-// TODO : test for shaders
+// TODO : tests for shaders
 
-fdescribe('writeResources', function() {
+describe('writeResources', function() {
     beforeEach(function(done) {
         var gltfLoaded = fsExtra.readJsonSync(gltfPath);
         return Promise.all([
@@ -44,22 +38,82 @@ fdescribe('writeResources', function() {
             }), done).toResolve();
     });
 
-    it('writes external resources', function(done) {
+    it('writes resources as files with default names', function(done) {
+        var externalResources = {};
         var options = {
             separateBuffers : true,
-            separateShaders : true,
             separateTextures : true,
-            externalResources : {}
+            externalResources : externalResources
         };
         expect(writeResources(gltf, options)
             .then(function(gltf) {
                 ForEach.image(gltf, function(image) {
                     expect(image.bufferView).toBeUndefined();
-                    expect(image.uri).toBe('cesium.png');
+                    expect(image.uri).toBe('image0.png');
                 });
                 expect(gltf.buffers.length).toBe(1);
                 var buffer = gltf.buffers[0];
-                expect(buffer.uri).toBe('0.bin'); // TODO : don't actually name 0.bin
+                expect(buffer.uri).toBe('buffer.bin');
+                expect(Object.keys(externalResources).length).toBe(2);
+                expect(Buffer.isBuffer(externalResources['buffer.bin']));
+                expect(Buffer.isBuffer(externalResources['image0.png']));
+            }), done).toResolve();
+    });
+
+    it('writes resources as files with object names', function(done) {
+        var externalResources = {};
+        var options = {
+            separateBuffers : true,
+            separateTextures : true,
+            externalResources : externalResources
+        };
+        gltf.buffers[0].name = 'my-buffer';
+        gltf.images[0].name = 'my-image';
+        expect(writeResources(gltf, options)
+            .then(function() {
+                expect(gltf.buffers[0].uri).toBe('my-buffer.bin');
+                expect(gltf.images[0].uri).toBe('my-image.png');
+            }), done).toResolve();
+    });
+
+    it('writes resources as files with gltf name', function(done) {
+        var externalResources = {};
+        var options = {
+            name : 'my-gltf',
+            separateBuffers : true,
+            separateTextures : true,
+            externalResources : externalResources
+        };
+        expect(writeResources(gltf, options)
+            .then(function() {
+                expect(gltf.buffers[0].uri).toBe('my-gltf.bin');
+                expect(gltf.images[0].uri).toBe('my-gltf0.png');
+            }), done).toResolve();
+    });
+    it('writes resources as data uris', function(done) {
+        var options = {
+            dataUris : true
+        };
+        expect(writeResources(gltf, options)
+            .then(function(gltf) {
+                var buffer = gltf.buffers[0];
+                var image = gltf.images[0];
+                expect(image.bufferView).toBeUndefined();
+                expect(Buffer.isBuffer(dataUriToBuffer(image.uri)));
+                expect(Buffer.isBuffer(dataUriToBuffer(buffer.uri)));
+            }), done).toResolve();
+    });
+
+    it('writes resources as bufferViews', function(done) {
+        expect(writeResources(gltf)
+            .then(function(gltf) {
+                var buffer = gltf.buffers[0];
+                var image = gltf.images[0];
+                var bufferViewByteLength = gltf.bufferViews[image.bufferView].byteLength;
+                var source = image.extras._pipeline.source;
+                expect(image.bufferView).toBeDefined();
+                expect(bufferViewByteLength).toBe(source.byteLength);
+                expect(Buffer.isBuffer(dataUriToBuffer(buffer.uri)));
             }), done).toResolve();
     });
 });
