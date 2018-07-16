8d68d72c9911c55211693a13acdc94164cb1926f
Sean Lilley <lilleyse@gmail.com>
2018-07-16 13:37:53 -0400

    Traverse glTF for extension

--- specs/lib/removeExtensionsSpec.js
+++ specs/lib/removeExtensionsSpec.js
@@ -19,13 +19,46 @@ describe('removeExtension', function() {
                     value: 9
                 },
                 extension2: [0, 1, 2]
-            }
+            },
+            materials: [
+                {
+                    baseColorFactor: [1.0, 0.0, 0.0, 1.0],
+                    extensions: {
+                        extension1: {
+                            value: 10
+                        }
+                    }
+                },
+                {
+                    baseColorFactor: [0.0, 0.0, 1.0, 1.0],
+                    extensions: {
+                        extension1: {
+                            value: 11
+                        }
+                    }
+                }
+            ],
+            cameras: [
+                {
+                    extensions: {
+                        extension1: {
+                            value: 9
+                        },
+                        extension2: [3, 4, 5]
+                    }
+                }
+            ]
         };
         var extension1 = removeExtension(gltf, 'extension1');
         expect(gltf.extensionsRequired).toEqual(['extension2', 'extension3']);
         expect(gltf.extensionsUsed).toEqual(['extension2', 'extension3']);
         expect(gltf.extensions).toEqual({
             extension2: [0, 1, 2]
+        });
+        expect(gltf.materials[0].extensions).toBeUndefined();
+        expect(gltf.materials[1].extensions).toBeUndefined();
+        expect(gltf.cameras[0].extensions).toEqual({
+            extension2: [3, 4, 5]
         });
         expect(extension1).toEqual({
             value: 9
@@ -35,12 +68,18 @@ describe('removeExtension', function() {
         expect(gltf.extensionsRequired).toEqual(['extension3']);
         expect(gltf.extensionsUsed).toEqual(['extension3']);
         expect(gltf.extensions).toBeUndefined();
-        expect(extension2).toEqual([0, 1,2]);
+        expect(gltf.materials[0].extensions).toBeUndefined();
+        expect(gltf.materials[1].extensions).toBeUndefined();
+        expect(gltf.cameras[0].extensions).toBeUndefined();
+        expect(extension2).toEqual([0, 1, 2]);
 
         var extension3 = removeExtension(gltf, 'extension3');
         expect(gltf.extensionsRequired).toBeUndefined();
         expect(gltf.extensionsUsed).toBeUndefined();
         expect(gltf.extensions).toBeUndefined();
+        expect(gltf.materials[0].extensions).toBeUndefined();
+        expect(gltf.materials[1].extensions).toBeUndefined();
+        expect(gltf.cameras[0].extensions).toBeUndefined();
         expect(extension3).toBeUndefined();
 
         var emptyGltf = {};
