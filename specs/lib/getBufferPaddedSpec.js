640f95251d575610f726234ab803ed11027a76b1
Sean Lilley <lilleyse@gmail.com>
2021-08-01 09:20:16 -0400

    Pad json, buffers, bufferViews to 8-bytes to support EXT_feature_metadata

--- specs/lib/getBufferPaddedSpec.js
+++ specs/lib/getBufferPaddedSpec.js
@@ -2,21 +2,25 @@
 const getBufferPadded = require('../../lib/getBufferPadded');
 
 describe('getBufferPadded', () => {
-    it('gets buffer padded to 4 bytes', () => {
+    it('gets buffer padded to 8 bytes', () => {
         let buffer = Buffer.alloc(0);
         let bufferPadded = getBufferPadded(buffer);
         expect(bufferPadded.length).toBe(0);
 
         buffer = Buffer.from([1]);
         bufferPadded = getBufferPadded(buffer);
-        expect(bufferPadded.length).toBe(4);
+        expect(bufferPadded.length).toBe(8);
         expect(bufferPadded.readUInt8(0)).toBe(1);
         expect(bufferPadded.readUInt8(1)).toBe(0);
         expect(bufferPadded.readUInt8(2)).toBe(0);
         expect(bufferPadded.readUInt8(3)).toBe(0);
+        expect(bufferPadded.readUInt8(4)).toBe(0);
+        expect(bufferPadded.readUInt8(5)).toBe(0);
+        expect(bufferPadded.readUInt8(6)).toBe(0);
+        expect(bufferPadded.readUInt8(7)).toBe(0);
 
-        // Does not allocate a new buffer when buffer length is already aligned to 4 bytes
-        buffer = Buffer.alloc(4);
+        // Does not allocate a new buffer when buffer length is already aligned to 8 bytes
+        buffer = Buffer.alloc(8);
         bufferPadded = getBufferPadded(buffer);
         expect(bufferPadded).toBe(buffer);
 
--- specs/lib/mergeBuffersSpec.js
+++ specs/lib/mergeBuffersSpec.js
@@ -10,8 +10,8 @@ describe('mergeBuffers', () => {
         const dataUri0 = 'data:application/octet-stream;base64,' + buffer0.toString('base64');
         const dataUri1 = 'data:application/octet-stream;base64,' + buffer1.toString('base64');
 
-        // All buffer views start on 4-byte alignment, the buffer ends on a 4-byte alignment, and extraneous buffer data is removed
-        const expectedBuffer = Buffer.from((new Uint8Array([1, 1, 1, 0, 2, 2, 0, 0, 3, 3, 3, 0, 4, 4, 4, 4, 4, 0, 0, 0])));
+        // All buffer views start on 8-byte alignment, the buffer ends on a 8-byte alignment, and extraneous buffer data is removed
+        const expectedBuffer = Buffer.from((new Uint8Array([1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 0, 0, 0])));
 
         const gltf = {
             bufferViews: [
@@ -69,10 +69,10 @@ describe('mergeBuffers', () => {
         const dataUri4 = 'data:application/octet-stream;base64,' + buffer4.toString('base64');
         const dataUri5 = 'data:application/octet-stream;base64,' + buffer5.toString('base64');
 
-        // All buffer views start on 4-byte alignment, the buffer ends on a 4-byte alignment, and extraneous buffer data is removed
-        const expectedBuffer0 = Buffer.from((new Uint8Array([1, 1, 1, 1, 2, 2, 2, 2])));
-        const expectedBuffer1 = Buffer.from((new Uint8Array([3, 0, 0, 0, 4, 4, 4, 0])));
-        const expectedBuffer2 = Buffer.from((new Uint8Array([5, 5, 0, 0, 6, 0, 0, 0, 7, 0, 0, 0])));
+        // All buffer views start on 8-byte alignment, the buffer ends on a 8-byte alignment, and extraneous buffer data is removed
+        const expectedBuffer0 = Buffer.from((new Uint8Array([1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0])));
+        const expectedBuffer1 = Buffer.from((new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0])));
+        const expectedBuffer2 = Buffer.from((new Uint8Array([5, 5, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0])));
 
         const gltf = {
             bufferViews: [
@@ -173,14 +173,14 @@ describe('mergeBuffers', () => {
         const dataUri4 = 'data:application/octet-stream;base64,' + buffer4.toString('base64');
         const dataUri5 = 'data:application/octet-stream;base64,' + buffer5.toString('base64');
 
-        // All buffer views start on 4-byte alignment, the buffer ends on a 4-byte alignment, and extraneous buffer data is removed
-        const expectedBuffer0 = Buffer.from((new Uint8Array([1, 1, 1, 1])));
-        const expectedBuffer1 = Buffer.from((new Uint8Array([2, 2, 2, 2])));
-        const expectedBuffer2 = Buffer.from((new Uint8Array([3, 0, 0, 0])));
-        const expectedBuffer3 = Buffer.from((new Uint8Array([5, 5, 0, 0])));
-        const expectedBuffer4 = Buffer.from((new Uint8Array([4, 4, 4, 0])));
-        const expectedBuffer5 = Buffer.from((new Uint8Array([6, 0, 0, 0])));
-        const expectedBuffer6 = Buffer.from((new Uint8Array([7, 0, 0, 0])));
+        // All buffer views start on 8-byte alignment, the buffer ends on a 8-byte alignment, and extraneous buffer data is removed
+        const expectedBuffer0 = Buffer.from((new Uint8Array([1, 1, 1, 1, 0, 0, 0, 0])));
+        const expectedBuffer1 = Buffer.from((new Uint8Array([2, 2, 2, 2, 0, 0, 0, 0])));
+        const expectedBuffer2 = Buffer.from((new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0])));
+        const expectedBuffer3 = Buffer.from((new Uint8Array([5, 5, 0, 0, 0, 0, 0, 0])));
+        const expectedBuffer4 = Buffer.from((new Uint8Array([4, 4, 4, 0, 0, 0, 0, 0])));
+        const expectedBuffer5 = Buffer.from((new Uint8Array([6, 0, 0, 0, 0, 0, 0, 0])));
+        const expectedBuffer6 = Buffer.from((new Uint8Array([7, 0, 0, 0, 0, 0, 0, 0])));
 
         const gltf = {
             bufferViews: [
