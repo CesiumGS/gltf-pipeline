182481f97fd1c5b231a8a105d855a4264e0fe800
Sean Lilley <lilleyse@gmail.com>
2021-08-02 13:47:50 -0400

    Fix JSON chunk alignment relative to start of glb

--- specs/lib/getJsonBufferPaddedSpec.js
+++ specs/lib/getJsonBufferPaddedSpec.js
@@ -18,4 +18,17 @@ describe("getJsonBufferPadded", () => {
     expect(bufferPadded.readUInt8(30)).toBe(32);
     expect(bufferPadded.readUInt8(31)).toBe(32);
   });
+
+  it("get json buffer padded to 8 bytes relative to byte offset", () => {
+    const gltf = {
+      asset: {
+        version: "2.0",
+      },
+    };
+    const string = JSON.stringify(gltf);
+    expect(string.length).toBe(27);
+    const bufferPadded = getJsonBufferPadded(gltf, 20);
+    expect(bufferPadded.length).toBe(28);
+    expect(bufferPadded.readUInt8(27)).toBe(32); // Space
+  });
 });
