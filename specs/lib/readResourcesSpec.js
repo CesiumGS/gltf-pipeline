20e268cdf3a51383391c1c9a7b6068ccca036dd9
Bryan Elliott <bryan.elliott@bentley.com>
2025-06-02 22:21:13 -0400

    Less agressive abs detection, well-known behavior with warning, docs

--- specs/lib/readResourcesSpec.js
+++ specs/lib/readResourcesSpec.js
@@ -5,6 +5,7 @@ const path = require("path");
 const ForEach = require("../../lib/ForEach");
 const parseGlb = require("../../lib/parseGlb");
 const readResources = require("../../lib/readResources");
+const { pathToFileURL, fileURLToPath } = require("url");
 
 const RuntimeError = Cesium.RuntimeError;
 
@@ -125,5 +126,64 @@ describe("readResources", () => {
         "glTF model references separate files but no resourceDirectory is supplied",
       ),
     );
+  });
+
+  it("rejects if gltf contains absolute resource paths, and there is a resourceDirectory", async () => {
+    const gltf = readGltf(boxTexturedSeparate2Path);
+    const localUri = gltf.buffers[0].uri;
+    const errMsg =
+      "Absolute paths are not permitted; use the allowAbsolute option to disable this error.";
+
+    // uri = file:///...
+    gltf.buffers[0].uri = new URL(
+      localUri,
+      pathToFileURL(boxTexturedSeparate2Path),
+    ).toString();
+    await expectAsync(
+      readResources(gltf, {
+        resourceDirectory: path.dirname(boxTexturedSeparate2Path),
+      }),
+    ).toBeRejectedWithError(RuntimeError, errMsg);
+  });
+
+  it("reads absolute resource paths when allowAbsolute there is no resourceDirectory", async () => {
+    const gltf = readGltf(boxTexturedSeparate2Path);
+    const toAbs = (uri) =>
+      new URL(uri, pathToFileURL(boxTexturedSeparate2Path)).toString();
+    spyOn(process.stderr, "write");
+    // uri = file:///...
+    gltf.buffers[0].uri = toAbs(gltf.buffers[0].uri);
+    gltf.images[0].uri = toAbs(gltf.images[0].uri);
+
+    await readResources(gltf);
+
+    expect(gltf.images[0]?.extras?._pipeline).toBeDefined();
+    expect(gltf.buffers[0]?.extras?._pipeline).toBeDefined();
+    // No options passed, but did an absolute path; expect a warning to have been emitted
+    expect(process.stderr.write).toHaveBeenCalled();
+    process.stderr.write.calls.reset();
+  });
+
+  it("reads absolute resource paths when allowAbsolute is true", async () => {
+    const gltf = readGltf(boxTexturedSeparate2Path);
+    const toAbs = (uri) =>
+      fileURLToPath(
+        new URL(uri, pathToFileURL(boxTexturedSeparate2Path)).toString(),
+      );
+    spyOn(process.stderr, "write");
+    // uri = /...
+    gltf.buffers[0].uri = toAbs(gltf.buffers[0].uri);
+    gltf.images[0].uri = toAbs(gltf.images[0].uri);
+
+    await readResources(gltf, {
+      allowAbsolute: true,
+      resourceDirectory: path.dirname(boxTexturedSeparate2Path),
+    });
+
+    expect(gltf.images[0]?.extras?._pipeline).toBeDefined();
+    expect(gltf.buffers[0]?.extras?._pipeline).toBeDefined();
+    // resourceDirectory present and allowAbsolute is true.  No warnings should be emitted.
+    expect(process.stderr.write).not.toHaveBeenCalled();
+    process.stderr.write.calls.reset();
   });
 });
